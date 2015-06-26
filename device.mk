03f6a1b740ef770478bfb91018107e0679557285
Edward Wang <ewang@cyngn.com>
2015-06-26 15:17:12 -0700

    jalebi: use correct animation resolution
    Change-Id: I21fe125537688e7011ec8b78c4f6056dd9013db7

--- device.mk
+++ device.mk
@@ -50,8 +50,8 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
