5db4c8a91b24a29a3e28285532bf713e31a73ca2
Ethan Chen <intervigil@gmail.com>
2015-07-14 15:32:33 -0700

    jalebi: Move ueventd back to common
    Change-Id: Iee991220da15a31777e4ae20db1d84f8f608d087

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,11 +19,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
