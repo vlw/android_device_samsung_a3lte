7d4de666e5551c5131d49ce2ec7b7a8d8735db36
Ethan Chen <intervigil@gmail.com>
2015-08-05 12:10:05 -0700

    jalebi: Move USB IDs to default properties
    Change-Id: I64b04b5001ffa08e5f189c73410bddde17d94a5d

--- device.mk
+++ device.mk
@@ -53,6 +53,17 @@ PRODUCT_PACKAGES += \
     camera.msm8916 \
     libmm-qcamera
 
+# Charger
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.usb.id.charge=3011 \
+    ro.usb.id.mtp=300B \
+    ro.usb.id.mtp_adb=300C \
+    ro.usb.id.ptp=300D \
+    ro.usb.id.ptp_adb=300E \
+    ro.usb.id.ums=300F \
+    ro.usb.id.ums_adb=3010 \
+    ro.usb.vid=2a96
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8916
