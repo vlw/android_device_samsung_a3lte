5d21d9c36361b1248d8d91f41538c8ba9a54f973
Steve Kondik <steve@cyngn.com>
2015-08-09 19:47:31 -0700

    jalebi: Use the correct memory configuration
    Change-Id: I5a9001f2a942b58c6a5f9f6c47579db47c33270d

--- device.mk
+++ device.mk
@@ -33,8 +33,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
