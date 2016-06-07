f37ab8e4cf3bb75f4881b98dee92eb8ce24d7093
Ethan Chen <intervigil@gmail.com>
2016-06-07 10:04:15 -0700

    jalebi: Enable CNE, DPM, QC location
    Change-Id: Iae6a2bd5b78367779afa2fb900968e77cf7efefd

--- device.mk
+++ device.mk
@@ -70,6 +70,10 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.usb.id.ums_adb=3010 \
     ro.usb.vid=2a96
 
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1
+
 # Display
 PRODUCT_PACKAGES += \
     libjni_livedisplay_qdcm \
@@ -86,6 +90,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.location \
+    ro.gps.agps_provider=1
 
 # Keylayout
 PRODUCT_COPY_FILES += \
