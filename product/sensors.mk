e2d22b75e6e9cd9a893e525530164e06c19c792f
jrior001 <jriordan001@gmail.com>
2018-02-14 21:23:17 -0500

    jalebi: move permissions to vendor
    Change-Id: Ibdc83b3a6a6367cebbfe7be4d1b97eef90cc8e8a

--- product/sensors.mk
+++ product/sensors.mk
@@ -5,6 +5,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/vendor/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/vendor/etc/permissions/android.hardware.sensor.proximity.xml \
+    $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/vendor/etc/permissions/handheld_core_hardware.xml
--- product/usb.mk
+++ product/usb.mk
@@ -1,6 +1,6 @@
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/vendor/etc/permissions/android.hardware.usb.host.xml
 
 # USB ID
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
