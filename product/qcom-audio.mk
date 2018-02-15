15568d8c4d7383cdadafe0a65aef813ba2a86d41
jrior001 <jriordan001@gmail.com>
2018-02-14 21:47:43 -0500

    jalebi: move audio configs to vendor
    Change-Id: I20bccd4bcf121789ac4a3ace7c8fa6583d358c0b

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -7,8 +7,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/QRD_Hdmi_cal.acdb:system/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/QRD_Headset_cal.acdb:system/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths_qrd_skui.xml:system/vendor/etc/mixer_paths_qrd_skui.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
