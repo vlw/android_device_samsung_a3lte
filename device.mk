8ff3eb9972fe2bf07f51e34b4b13791195458511
Michael Bestas <mkbestas@lineageos.org>
2018-06-14 17:16:07 +0300

    jalebi: Move keylayout to vendor
    Change-Id: Id95f014cad3200b23b48861a72dd71e5ed2ec35a

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/vendor/usr/keylayout/gpio-keys.kl
 
 # Lights
 PRODUCT_PACKAGES += \
