97034d5ef0856d62114f50d256e3752dbafa209d
Dev4Perf <dev4perf@gmail.com>
2018-08-10 22:12:43 +0200

    jalebi: Consider lineage_jalebi product via the AOSP way
    Change-Id: I684c7994831e1a6a100c4bf9ed1f6c47f8d9bb63

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +15,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_jalebi.mk
+    $(LOCAL_DIR)/lineage_jalebi.mk
