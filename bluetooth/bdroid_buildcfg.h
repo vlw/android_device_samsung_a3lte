c9e80f3ad21deba7d55118390a378d6e8ce16e76
vlw <vlwwwwww@gmail.com>
2019-05-11 03:23:28 +0300

    bt-config: Update to A3

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME        "YU4711"
+#define BTM_DEF_LOCAL_NAME        "Galaxy A3"
 
 #define BLE_VND_INCLUDED          TRUE
 #define BLUETOOTH_QTI_SW          TRUE
